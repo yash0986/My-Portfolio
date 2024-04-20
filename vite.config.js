1d02412348bc82ab0033ae9d4ecc8f4274bb4584
yash0986 <116620799+yash0986@users.noreply.github.com>
2024-04-21 01:47:50 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myPortfolio/",
+  base: "/My-Portfolio/",
   plugins: [react()],
   css: {
     modules: {
